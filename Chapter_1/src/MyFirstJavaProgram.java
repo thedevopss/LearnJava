6deccf76b35985a9ff8c802d29bbf3cf94efb5b6
Thakur Adhikari <thakurad@Thakurs-MBP.hub>
2020-08-18 23:08:25 +1000

    Chapter-1: Added Hello world and data type

---
+++ Chapter_1/src/MyFirstJavaProgram.java
@@ -0,0 +1,16 @@
+public class MyFirstJavaProgram {
+    public static void main (String[] args){
+        /* This is my first java program.
+         * This will print 'Hello World' as the output
+         */
+
+        System.out.print("Hello World");
+
+        // data type
+        String test = "test"; // string
+        int num= 1;
+        float myFloat = 5.66f;
+        char myLetter = 'D';
+        boolean myBool = true;
+    }
+}
